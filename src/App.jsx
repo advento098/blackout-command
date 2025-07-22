76b82724ed78693b4ed7c1a9b3f1d50d5f73cfdd
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-07-22 09:53:07 +0800

    Added the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <nav>
+          <div>
+            <ul>
+              <li className="brand">BLACKOUT INC.</li>
+              <li>Home</li>
+              <li>About</li>
+            </ul>
+          </div>
+          <div className="hexagon-border">
+            <div className="hexagon-fill"></div>
+          </div>
+          <div>
+            <ul>
+              <li>Inventory</li>
+              <li>Shop</li>
+              <li className="game-info">
+                Game Info
+                <span class="material-symbols-outlined">arrow_drop_down</span>
+                <div className="options">
+                  <button>Weapons</button>
+                  <button>Disguise</button>
+                  <button>Customization</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <section>
+          <div className="title-container">
+            <h4>NO POWER. NO MERCY. JUST SURVIVAL</h4>
+            <h1>
+              BLACKOUT <br />
+              <span>COMMAND</span>
+            </h1>
+          </div>
+          <div className="bottom-panel">
+            <div>
+              <div className="copyright"></div>
+              <div className="CTA-button"></div>
+              <div className="search-bar"></div>
+            </div>
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
