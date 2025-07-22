430928fd72e6fa4486eea710ef3eea44d32585ca
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-07-22 14:52:16 +0800

    Added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,37 @@ function App() {
   return (
     <>
       <div className="main-container">
-        <nav>
-          <div>
-            <ul>
-              <li className="brand">BLACKOUT INC.</li>
-              <li>Home</li>
-              <li>About</li>
-            </ul>
-          </div>
-          <div className="hexagon-border">
-            <div className="hexagon-fill"></div>
-          </div>
-          <div>
-            <ul>
-              <li>Inventory</li>
-              <li>Shop</li>
-              <li className="game-info">
-                Game Info
-                <span class="material-symbols-outlined">arrow_drop_down</span>
-                <div className="options">
-                  <button>Weapons</button>
-                  <button>Disguise</button>
-                  <button>Customization</button>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <div>
+          <nav>
+            <div>
+              <ul>
+                <li className="brand">BLACKOUT INC.</li>
+                <li>Home</li>
+                <li>About</li>
+              </ul>
+            </div>
+            <div className="hexagon-border">
+              <div className="hexagon-fill">
+                <img src="./src/assets/controller-logo.svg" />
+              </div>
+            </div>
+            <div>
+              <ul>
+                <li>Inventory</li>
+                <li>Shop</li>
+                <li className="game-info">
+                  Game Info
+                  <span class="material-symbols-outlined">arrow_drop_down</span>
+                  <div className="options">
+                    <button>Weapons</button>
+                    <button>Disguise</button>
+                    <button>Customization</button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
         <section>
           <div className="title-container">
             <h4>NO POWER. NO MERCY. JUST SURVIVAL.</h4>
@@ -46,8 +50,16 @@ function App() {
             <div className="vignette"></div>
             <img src="./src/assets/character-model.svg" alt="soldier-model" />
             <div>
-              <div className="copyright">&copy; Blackout Inc.</div>
-              <div className="CTA-button"></div>
+              <div className="copyright">
+                &copy; Blackout Inc.
+                <span />
+              </div>
+              <div className="CTA-button">
+                <div>
+                  <img src="./src/assets/button.svg"></img>
+                  <p>Download Now</p>
+                </div>
+              </div>
               <div className="search-bar"></div>
             </div>
           </div>
