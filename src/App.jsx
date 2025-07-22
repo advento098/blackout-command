c391971f5d75d4ae7dcdb94ad3b8a08166c580b1
Pons Anthony Advento <adventopr@students.nu-fairview.edu.ph>
2025-07-22 20:10:57 +0800

    Added side drawer navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Nav from "./components/Nav";
+import Content from "./components/Content";
 
 function App() {
   return (
     <>
-      <div>
-        <nav>
-          <div>
-            <ul>
-              <li className="brand">BLACKOUT INC.</li>
-              <li>Home</li>
-              <li>About</li>
-            </ul>
-          </div>
-          <div className="hexagon-border">
-            <div className="hexagon-fill">
-              <img src="./src/assets/controller-logo.svg" />
-            </div>
-          </div>
-          <div>
-            <ul>
-              <li>Inventory</li>
-              <li>Shop</li>
-              <li className="game-info">
-                Game Info
-                <span class="material-symbols-outlined">arrow_drop_down</span>
-                <div className="options">
-                  <button>Weapons</button>
-                  <button>Disguise</button>
-                  <button>Customization</button>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-      <div>
-        <div className="title-container">
-          <h4>NO POWER. NO MERCY. JUST SURVIVAL.</h4>
-          <h1>
-            BLACKOUT <br />
-            <span>COMMAND</span>
-          </h1>
-        </div>
-        <div className="bottom-panel">
-          <div className="vignette"></div>
-          <img src="./src/assets/character-model.svg" alt="soldier-model" />
-          <div>
-            <div className="copyright">
-              &copy; Blackout Inc.
-              <span />
-            </div>
-            <div className="CTA-button">
-              <div>
-                <img src="./src/assets/button.svg"></img>
-                <p>Download Now</p>
-              </div>
-            </div>
-            <div className="search-bar">
-              <div>
-                <input type="text" placeholder="Search arsenal" />
-                <span class="material-symbols-outlined">search</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Nav></Nav>
+      <Content></Content>
     </>
   );
 }
